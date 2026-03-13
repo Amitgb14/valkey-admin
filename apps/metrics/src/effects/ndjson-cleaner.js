b9df5b9bc40e5f00ad5320ac00eefb289cbaafdf
Argus Li <contactme@chunkeili.com>
2026-03-12 21:46:48 -0700

    Add comments.
    Signed-off-by: Argus Li <contactme@chunkeili.com>

--- apps/metrics/src/effects/ndjson-cleaner.js
+++ apps/metrics/src/effects/ndjson-cleaner.js
@@ -12,14 +12,17 @@ let cleanerStopper
 export const setupNdjsonCleaner = ( cfg ) => {
   const pipeline$ = timer(0, METRICS_EVICTION_POLICY.INTERVAL).pipe(
     exhaustMap(() => 
-      fs.promises.readdir(cfg.server.data_dir)
+      fs.promises.readdir(cfg.server.data_dir) // get file names from directory
+        // get file stats for each file
         .then((fileNames) => Promise.all(fileNames.map(
           async (fileName) => ({ stats: await fs.promises.stat(path.join(cfg.server.data_dir, fileName)), fileName }),
         )))
+        // filter out files that are not expired
         .then((filesWithStats) => filesWithStats.reduce((acc, { stats, fileName }) => { 
           if (stats.birthtime < Date.now() - cfg.storage.retention_days * MILLISECONDS_IN_A_DAY) acc.push(fileName)
           return acc
         }, []))
+        // delete expired files
         .then(async (expiredFileNames) => (await Promise.allSettled(expiredFileNames.map(
           (fileName) => fs.promises.unlink(path.join(cfg.server.data_dir, fileName)),
         )))
