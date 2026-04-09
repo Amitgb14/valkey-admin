6e7228120a770fc24c18eb74af3f12c4447b66a5
Argus Li <contactme@chunkeili.com>
2026-04-09 13:42:24 -0700

    Remove unnecessary payloadConnectionDetails.
    Signed-off-by: Argus Li <contactme@chunkeili.com>

--- apps/server/src/connection.ts
+++ apps/server/src/connection.ts
@@ -121,7 +121,6 @@ export async function connectToValkey(
       payload: {
         connectionId,
         connectionDetails: {
-          ...payload.connectionDetails,
           keyEvictionPolicy,
           jsonModuleAvailable,
         },
